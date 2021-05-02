e23c7172cc603b8f605a0b2b083bde26618ab306
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-02 09:32:21 +0600

    carousal done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import MyCarousal from "./components/MyCarousal/MyCarousal";
+import MyNavbar from "./components/MyNavbar/MyNavbar";
+import TitleMessage from "./components/TitleMessage/TitleMessage";
 
 const App = () => {
-  return <div className="App">My Portfolio</div>;
+  return (
+    <div>
+      <MyNavbar />
+      <MyCarousal />
+      <TitleMessage />
+    </div>
+  );
 };
 
 export default App;
