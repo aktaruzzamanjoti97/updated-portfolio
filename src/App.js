3bbdfae4404fcb79d7183f022b62d716e63948ce
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-05-02 11:33:40 +0600

    about section updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import "./App.css";
 import MyCarousal from "./components/MyCarousal/MyCarousal";
 import MyNavbar from "./components/MyNavbar/MyNavbar";
 import TitleMessage from "./components/TitleMessage/TitleMessage";
+import AboutMe from "./pages/AboutMe/AboutMe";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
   return (
-    <div>
+    <div  className="App" style={{ position: "relative" }}>
       <MyNavbar />
       <MyCarousal />
       <TitleMessage />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/background/background.webp")}
+          bgImageAlt=""
+          strength={200}
+        >
+          <div>
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <AboutMe />
+            </Fade>
+          </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 };
---
+++ src/pages/AboutMe/AboutMe.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+import Profile from '../../assets/img/joti4.jpg';
+import './AboutMe.css';
+
+const AboutMe = () => {
+    return (
+        <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3 text-bold">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Aktaruzzaman Joti</strong>
+                <br />A passionate programmer and a Full Stack Web Developer with React.js, Redux, Express.js, Node.js as my tech stack.
+                <br />
+                Proactive M.E.R.N Stack Developer seeking opportunity to bring knowledge of Programming, Design, and media to Web Development position with Companies. 
+                <br />
+                Working with the clients, my goal is always driven towards providing amazing experience with the best level of quality and service to them.
+                <br />
+                Along with that, I also help people as a COACH on their journey of becoming a professional programmer. 
+                <br />Looking for opportunities to make the best use of my skills and create great products.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://drive.google.com/file/d/1tm4JoX9elehn5lYSRYZUMQPJbZuYQhGx/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/aktaruzzamanjoti97" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/aktaruzzaman-joti/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+    );
+};
+
+export default AboutMe;
